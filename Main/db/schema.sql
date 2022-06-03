5e2b844502dc14aa8ee7a574579be264fdc94b24
Trevor Hilimire <trevorhilimire@gmail.com>
2022-06-03 17:02:29 -0400

    builds objects from db and logic for question choices and makes responses into tables. Adds schema

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,1 +1,150 @@
 CREATE DATABASE employee_tracker;
+
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: employee_tracker
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `role_id` int NOT NULL,
+  `manager_id` int NOT NULL,
+  PRIMARY KEY (`id`,`role_id`),
+  KEY `id_idx` (`manager_id`),
+  CONSTRAINT `id` FOREIGN KEY (`manager_id`) REFERENCES `employee` (`id`),
+  CONSTRAINT `role_id` FOREIGN KEY (`id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `employee_view`
+--
+
+DROP TABLE IF EXISTS `employee_view`;
+/*!50001 DROP VIEW IF EXISTS `employee_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `employee_view` AS SELECT 
+ 1 AS `first_name`,
+ 1 AS `last_name`,
+ 1 AS `title`,
+ 1 AS `salary`,
+ 1 AS `department_name`,
+ 1 AS `manager_name`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `salary` int NOT NULL,
+  `department_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `department_id_idx` (`department_id`),
+  CONSTRAINT `department_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `role_view`
+--
+
+DROP TABLE IF EXISTS `role_view`;
+/*!50001 DROP VIEW IF EXISTS `role_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `role_view` AS SELECT 
+ 1 AS `role_id`,
+ 1 AS `title`,
+ 1 AS `salary`,
+ 1 AS `department_name`,
+ 1 AS `department_id`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `employee_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `employee_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `employee_view` AS select `employee`.`first_name` AS `first_name`,`employee`.`last_name` AS `last_name`,`role`.`title` AS `title`,`role`.`salary` AS `salary`,`department`.`name` AS `department_name`,concat_ws(', ',`e2`.`last_name`,`e2`.`first_name`) AS `manager_name` from (((`employee` join `role` on((`employee`.`role_id` = `role`.`id`))) join `department` on((`role`.`department_id` = `department`.`id`))) join `employee` `e2` on((`e2`.`id` = `employee`.`manager_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `role_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `role_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `role_view` AS select `r`.`id` AS `role_id`,`r`.`title` AS `title`,`r`.`salary` AS `salary`,`d`.`name` AS `department_name`,`d`.`id` AS `department_id` from (`role` `r` join `department` `d` on((`r`.`department_id` = `d`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-03 16:59:43
---
+++ Main/index.js
@@ -0,0 +1,322 @@
+let questions = [];
+const inquirer = require("inquirer");
+const sequelize = require('../config/connection');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      password: 'sqlPr0gress!',
+      database: 'employee_tracker'
+    //   user: process.env.DB_USER,
+    //   // TODO: Add MySQL password
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employee tracker database.`)
+  );
+
+// Global Variables
+let departList = [];
+let roleList = [];
+let employeeList = [];
+
+async function loadGlobalData() {
+    pDepartList = loadDepartments();
+    pRoleList = loadRoles();
+    pEmployeeList = loadEmployees();
+    await Promise.all([pDepartList, pRoleList, pEmployeeList])
+    // console.log(typeof (Object.values(departList)));
+    console.log(departList);
+    // console.log(choiceConstructor(roleList));
+    createQuestions()
+    init();
+};
+loadGlobalData();
+
+async function loadDepartments(){
+    const sql = `SELECT id, name FROM department ORDER BY name`;
+    var results = await doSql(sql)
+    var currentList = [];
+
+    results.forEach(element => {
+            currentList.push({name: element.name, id: element.id})      
+        });
+    departList = currentList;
+};
+
+async function loadRoles(){
+    const sql = `SELECT * FROM role_view`;
+    var results = await doSql(sql)
+    var currentList = [];
+
+    results.forEach(element => {
+            currentList.push({title: element.title, id: element.role_id, salary: element.salary, department: element.department_id, department_name: element.department_name})     
+        });
+    roleList = currentList;
+};
+
+async function loadEmployees(){
+    const sql = `SELECT * FROM employee`;
+    var results = await doSql(sql)
+    var currentList = [];
+
+    results.forEach(element => {
+            currentList.push({id: element.id, first_name: element.first_name, last_name: element.last_name, role_id: element.role_id, manager_id: element.manager_id})      
+        });
+    employeeList = currentList;
+};
+
+async function doSql(sql)
+{
+    var results = await db.promise().query(sql);
+    return results[0];
+};
+
+function roleChoiceConstructor (dbList) {
+    return dbList.map(function(element)
+    {
+          return  {
+                name: element.title,
+                value: element
+            }
+        }
+    );
+};
+
+function departmentChoiceConstructor (dbList) {
+    return dbList.map(function(element)
+    {
+          return  {
+                name: element.name,
+                value: element
+            }
+        }
+    );
+};
+
+function employeeChoiceConstructor (dbList) {
+    return dbList.map(function(element)
+    {
+          return  {
+                name:"ID: " + element.id + "  "+ element.first_name + " " + element.last_name,
+                value: element
+            }
+        }
+    );
+};
+
+function createQuestions()
+{
+
+    questions = [
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', "Update an employee's role"]
+            },
+        {
+            type: "input",
+            name: "add_department",
+            message: "What is the name of the department you would like to add?",
+            when: (answers) => {
+                if (answers.action === "add a department") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "add_role",
+            message: "What is the name of the role you would like to add?",
+            when: (answers) => {
+                if (answers.action === "add a role") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "add_role_salary",
+            message: "What is the salary of this role",
+            when: (answers) => {
+                if (answers.action === "add a role") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "add_role_department",
+            message: "Which department does this role belong to?",
+            choices: departmentChoiceConstructor(departList),
+            when: (answers) => {
+                if (answers.action === "add a role") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "add_first_name_employee",
+            message: "What is the first name of the employee you would like to add?",
+            when: (answers) => {
+                if (answers.action === "add an employee") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "add_last_name_employee",
+            message: "What is the last name of the employee you would like to add?",
+            when: (answers) => {
+                if (answers.action === "add an employee") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "add_employee_role",
+            message: "What role does this employee have?",
+            choices: roleChoiceConstructor(roleList),
+            when: (answers) => {
+                if (answers.action === "add an employee") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "add_employee_manager",
+            message: "Whom is this person's manager?",
+            choices: employeeChoiceConstructor(employeeList),
+            when: (answers) => {
+                if (answers.action === "add an employee") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "update_employee",
+            message: "Which employee's role would you like to update?",
+            choices:  employeeChoiceConstructor (employeeList),
+            when: (answers) => {
+                if (answers.action === "Update an employee's role") {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "update_employee_role",
+            message: "Which role would you like to assing to this employee?",
+            choices:roleChoiceConstructor(roleList),
+            when: (answers) => {
+                if (answers.action === "Update an employee's role") {
+                    return true;
+                }
+            }
+        }
+    ];
+
+}
+async function init() {
+    var data = await inquirer.prompt(questions);
+    handleAnswers(data);
+};
+
+function handleAnswers(data) {
+    switch(data.action){
+        case 'view all departments':
+            console.log('list of departments: ')
+            console.table(departList, ["id", "name"])
+            init();
+            break;
+        case 'view all roles':
+            console.log('list of roles: ')
+            console.table(roleList)
+            init();
+            break;
+        case 'view all employees':
+            console.log('list of employees: ')
+            console.table(employeeList);
+            init();
+            break;
+        case 'add a department':
+            add_department(data.add_department);
+            init();
+            break;
+        case 'add a role':
+            add_role(data.add_role, data.add_role_salary, data.add_role_department.id );
+            init();
+            break;
+        case 'add an employee':
+            add_employee(data.add_first_name_employee, data.add_last_name_employee, data.add_employee_role.id, data.add_employee_manager.id);
+            init();
+            break;
+        case "Update an employee's role":
+            update_employee(data.update_employee.id, data.update_employee_role.id);
+            init();
+            break;
+    }
+
+    // console.log(data.action)
+};
+
+function add_department(department_name){
+    const sql = `INSERT INTO department (name) VALUES (?)`;
+            db.query(sql, department_name);
+            loadDepartments();
+            console.log("Department successfully added!");
+};
+
+function add_role(role, salary, department){
+    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+            try {db.query(sql, [role, salary, department], (error, results,fields)=> {
+                loadRoles();
+                console.log(error)
+
+            });
+           
+            }catch(e){
+                console.log(e)
+            }
+            console.log("Role successfully added!");
+};
+
+function add_employee(first, last, role, manager_id){
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+            try { db.query(sql, [first, last, role, manager_id], (error, results,fields)=> {
+            loadEmployees();
+            console.log(error)
+
+        });
+       
+        }catch(e){
+            console.log(e)
+        }
+            console.log("Employee successfully added!");
+};
+
+function update_employee(id, role_id){
+    const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+            try { db.query(sql, [role_id, id], (error, results,fields)=> {
+            loadEmployees();
+            console.log(error)
+
+        });
+       
+        }catch(e){
+            console.log(e)
+        }
+            console.log("Employee successfully added!");
+
+
+}
